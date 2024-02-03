57f3cfff05b1b100b4c962e21c2b8c1ca12783a5
Kirill Tarasov <kir_tarasov@mail.ru>
2024-02-03 17:13:55 +0300

    feat: add params

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 import os
 import sys
 
+
 import requests
+from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtCore import Qt, QPoint
 
 SCREEN_SIZE = [600, 450]
 
 
-class MapsAPI(QWidget):
+class MapsAPI(QMainWindow):
     def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
         self.ln = 37.530887
         self.lt = 55.703118
         self.spn = [0.002, 0.002]
         self.idx = 0
         self.l = 'map'
+        self.mark = None
+        self.mark = [self.ln, self.lt]
 
-        super().__init__()
+        self.searchBtn.clicked.connect(self.search)
+        self.resetBtn.clicked.connect(self.reset)
+
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={str(self.ln)},{str(self.lt)}&spn={str(self.spn[0])},{str(self.spn[1])}&l={self.l}"
-        response = requests.get(map_request)
+        url_mark = ''
+        if self.mark:
+            url_mark = f'&pt={self.mark[0]},{self.mark[1]}'
+
+        params = {
+            'll': f'{str(self.ln)},{str(self.lt)}',
+            'spn': f'{str(self.spn[0])},{str(self.spn[1])}',
+            'l': f'{self.l}',
+            'pt': f'{self.mark[0]},{self.mark[1]}',
+        }
+        map_request = f"http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_request, params=params)
 
         if not response:
             print("Ошибка выполнения запроса:")
@@ -40,9 +59,9 @@ class MapsAPI(QWidget):
         self.setWindowTitle('Отображение карты')
 
         self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
+        # self.image = QLabel(self)
+        # self.image.move(0, 0)
+        # self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
 
     def keyPressEvent(self, event):
@@ -80,6 +99,13 @@ class MapsAPI(QWidget):
         self.getImage()
         self.pixmap = QPixmap(self.map_file)
         self.image.setPixmap(self.pixmap)
+
+    def search(self):
+        print('search')
+
+
+    def reset(self):
+        print('reset')
 
     def closeEvent(self, event):
         os.remove(self.map_file)
